7070a4a1f59eaf8f41c8aab49cf837986fb45b00
tuscan <bmcgee08@gmail.com>
2022-04-07 13:40:15 -0500

    Add files via upload

---
+++ src0/calculator0.py
@@ -0,0 +1,11 @@
+# Demonstrates addition
+
+x = 1
+y = 2
+
+z = x + y
+
+print(z)
+
+
+
---
+++ src0/m2.py
@@ -0,0 +1,6 @@
+while True:
+    n = int(input("What's n? "))
+    if n > 0:
+        break
+for _ range(n):
+    print("meow")
